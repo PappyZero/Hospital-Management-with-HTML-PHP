1bbb5e659acfe7dc1ff336e038a54d990fcd95f2
PappyZero <amaddinmajid24@gmail.com>
2024-02-06 14:07:10 +0100

    The first commit for hms

---
+++ include/header.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS File -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <!-- Stackpath CSS -->
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+    <!-- Online Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- Local Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="../css/bootstrap-css/bootstrap.css"> -->
+
+    <!-- Font Awesome -->    
+    <!-- <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+    
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css"> -->
+
+    <!-- Font Awesome -->
+    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet" /> -->
+
+    <title>HEADER</title>
+</head>
+<body>        
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand brand_logo" href="#">
+                    <img src="./images/logo/7_removebg.png" alt="Logo" width="100" height="90" class="d-inline-block" >
+                    <span>pappy.care</span>
+                </a>
+
+
+                <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link active text-success" aria-current="page" href="#"><i class="fa-solid fa-map-location">  Location</i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-success" href="#"><i class="fa-solid fa-envelope">  Mail</i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-success" href="#"><i class="fa-solid fa-phone-volume">  Phone</i></a>
+                    </li>
+                </ul>
+
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <button class="btn btn-outline-success me-md-2" type="button">Sign In</button>
+                    <button class="btn btn-outline-success" type="button">Get Started</button>
+                </div>
+            </div>
+        </nav>
+
+        <nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary">
+            <div class="container">
+                                
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+                        <li class="nav-item nav_tabs">
+                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item nav_tabs">
+                            <a class="nav-link" href="admin_login.php">Admin</a>
+                        </li>
+                        <li class="nav-item nav_tabs dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Doctor</a>
+                            <ul class="dropdown-menu dropdown_menu_list">
+                                <li><a href="#" class="dropdown-item">Action 1</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a href="#" class="dropdown-item">Action 2</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a href="#" class="dropdown-item">Action 3</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item nav_tabs dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Receptionist</a>
+                            <ul class="dropdown-menu dropdown_menu_list">
+                                <li><a href="#" class="dropdown-item">Action 1</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a href="#" class="dropdown-item">Action 2</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a href="#" class="dropdown-item">Action 3</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item nav_tabs dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Patient</a>
+                            <ul class="dropdown-menu dropdown_menu_list">
+                                <li><a href="#" class="dropdown-item">Action 1</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a href="#" class="dropdown-item">Action 2</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a href="#" class="dropdown-item">Action 3</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                    
+                    <div class="d-grid gap-5 d-md-flex justify-content-md-end social_icons">
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                        <a href="#"><i class="fa-solid fa-lock"></i></a>
+                        <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+                    </div>
+                </div>
+
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+ 
+
+            </div>
+        </nav>
+
+    <!-- <nav class="nav nav-pills flex-column flex-sm-row">
+        <a class="flex-sm-fill text-sm-center nav-link active" aria-current="page" href="#">Active</a>
+        <a class="flex-sm-fill text-sm-center nav-link" href="#">Longer nav link</a>
+        <a class="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
+        <a class="flex-sm-fill text-sm-center nav-link disabled" aria-disabled="true">Disabled</a>
+    </nav> -->
+    
+</body>
+<!-- Font Awesome -->
+<script src="https://kit.fontawesome.com/6f1202466a.js" crossorigin="anonymous"></script>
+
+<!-- JQUERY -->
+<script src="https://code.jquery.com/jquery-3.7.1.slim.js" integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
+
+<!-- Google CDN -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+<!-- Online Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+<!-- Local Bootstrap JS -->
+<!-- <script src="../js/bootstrap-js/bootstrap.js"></script> -->
+</html>
---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <!--    CSS File  -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title> PAPPY HOSPITAL MANAGEMENT SYSTEM </title>
+</head>
+<body>
+<?php
+include_once("./include/header.php");
+?>
+    <div id="carouselExampleCaptions" class="carousel carousel-dark slide carousel-fade" data-bs-ride="true">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
+        </div>
+
+        <div class="carousel-inner carousel_inner_img">
+            <div class="card text-dark carousel-item carousel_item_img active" data-bs-interval="3000">
+                <img src="./images/med-recep.jpg" class="card-img" alt="...">
+
+                <div class="card-img-overlay bg_image_best img-thumbnail rounded">
+                    <h1 class="card-title">Best Choice For <br> Medical Health Care</h1>
+                    <p class="card-text">We learn about the most ideal option or healthcare <br> provider to ensure optimal outcomes, taking into <br> consideration factors such as quality of care, patient <br> preferences, and the effectiveness of medical practices.</p>
+                </div>
+            </div>
+
+            <div class="carousel-item carousel_item_img" data-bs-interval="3000">
+                <img src="./images/admin1.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>ADMINISTRATION</h5>
+                    <p>Administer your website with ease and precision.</p>
+                </div>
+            </div>
+
+            <div class="carousel-item carousel_item_img" data-bs-interval="3000">
+                <img src="images/patient3.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>DOCTOR</h5>
+                    <p>Enhance lives, we're hiring doctors.</p>
+                </div>
+            </div>
+
+            <div class="carousel-item carousel_item_img" data-bs-interval="3000">
+                <img src="./images/Medical-Receptionist.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>MEDICAL ADMINISTRATION</h5>
+                    <p>Elevate patient experience with Medical Reception.</p>
+                </div>
+            </div>
+
+            <div class="carousel-item carousel_item_img" data-bs-interval="3000">
+                <img src="./images/nurse4.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>NURSE STATION</h5>
+                    <p>Delivering care with compassion, one click away.</p>
+                </div>
+            </div>
+
+            <div class="carousel-item carousel_item_img" data-bs-interval="3000">
+                <img src="./images/patient6.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>PATIENT</h5>
+                    <p>Empowering patients with care, click for support.</p>
+                </div>
+            </div>
+            
+        </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+
+    </div>
+
+
+    <section class="section_img1">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-1">
+                    <img src="./images/wood.png" alt="">
+                </div>
+                <div class="col-md-2 bg_image_wood img-thumbnail rounded">
+                    <h1 class="card-title">Best Choice For <br> Medical Health Care</h1>
+                    <p class="card-text">We learn about the most ideal option or healthcare <br> provider to ensure optimal outcomes, taking into <br> consideration factors such as quality of care, patient <br> preferences, and the effectiveness of medical practices.</p>
+                    <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                </div>
+                <div class="col-md-8">
+                    <img src="./images/patient2.png" class="img-fluid" alt="...">
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container-fluid text-center">
+                <div class="card mb-3" style="width: 100%;">
+                    <div class="row g-0">
+                        <img src="./images/Specialized_Training.png" class="img-fluid" alt="...">
+                    </div>
+                </div>  
+        </div>
+    
+</body>
+</html>
